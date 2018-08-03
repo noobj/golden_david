ecbda6f6e3b35bec80c952690cde8bd499597dbd
JJJ <jingfu99@gmail.com>
2018-08-03 23:35:50 +0800

    add function move

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+
+// Define the maximum of the border
+#define MAX_WIDTH 4
+#define MAX_HEIGHT 2
+
+// to record where the player at now
+typedef struct Location {
+    int x;
+    int y;
+} Location;
+
+// function prototypes
+void move(Location *);
+
 
 int main() {
-	printf("FUCK YOU OUT\n");
+	Location player1 = {0,0};
+	move(&player1);
+
+	printf("FUCK YOU OUT (%d, %d)\n", player1.x, player1.y);
 	return 0;
+}
+
+void move(Location* player) {
+	(*player).y += 4;
 }
