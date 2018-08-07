a3c00748fc293b6e37116ecd1f9def9512467bd8
JJJ <jingfu99@gmail.com>
2018-08-07 17:55:52 +0800

    new feature KICK, fix #3

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Define the maximum of the border
 #define MAX_WIDTH 4
@@ -12,13 +13,14 @@ typedef struct Location {
 
 // function prototypes
 void move(Location *);
-
+void kick(Location *, Location *);
 
 int main() {
 	Location player1 = {0,0};	// init the player's location
+	Location ball = {0,0};	// init the ball's location
 	char action = 'm';	// to decide what kind of action you take, move by default
 	while(1) {
-		printf("PLAYER1: (%d, %d)\n", player1.x, player1.y);
+		printf("PLAYER1: (%d, %d)   BALL: (%d, %d)\n", player1.x, player1.y, ball.x, ball.y);
 		printf("kick(k) or move(m)?\n");
 		scanf(" %c", &action);
 
@@ -28,6 +30,7 @@ int main() {
 				move(&player1);
 				break;
 			case 'k':
+				kick(&player1, &ball);
 				break;
 			default:
 				move(&player1);
@@ -39,4 +42,19 @@ int main() {
 
 void move(Location* player) {
 	(*player).y += 4;
+}
+
+void kick(Location* player, Location* ball) {
+	if ((*player).x != (*ball).x || (*player).y != (*ball).y) {
+		printf("you dont have the ball\n");
+		return;
+	}
+
+	int min = (*player).x;		// use player's x position as the minimum random value
+	int randomX = (rand() % (MAX_WIDTH - min)) + min + 1;	// for store the random kick value of X
+	int randomY = rand() % (MAX_HEIGHT - 0);	// for store the random kick value of Y
+	(*ball).x = randomX;
+	(*ball).y = randomY;
+
+	printf("you've kicked the ball to (%d,%d)\n", randomX, randomY);
 }
